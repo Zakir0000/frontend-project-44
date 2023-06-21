b6630834e2f1e9951a3a557240a4b17429fdfc1b
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-06-21 19:29:36 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
- 
-export function greetUser() {
- 
+
+export default function greetUser() {
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
 }

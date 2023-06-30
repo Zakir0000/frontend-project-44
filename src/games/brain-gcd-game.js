e2038edf9ff8a15a75cdf1161d0937dd83ffe11e
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-06-30 14:03:53 +0300

    fix brain-gcd

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -3,22 +3,22 @@ import generateRandomNumber from '../generate-random-num.js';
 
 const gameTerms = 'Find the greatest common divisor of given numbers.';
 
+const calculateGcd = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+  return calculateGcd(num2, num1 % num2);
+};
+
+const playGameStructure = () => {
+  const num1 = generateRandomNumber();
+  const num2 = generateRandomNumber();
+  const question = `${num1} ${num2}`;
+
+  const correctAnswer = String(calculateGcd(num1, num2));
+  return [question, correctAnswer];
+};
+
 export default () => {
-  const calculateGcd = (num1, num2) => {
-    if (num2 === 0) {
-      return num1;
-    }
-    return calculateGcd(num2, num1 % num2);
-  };
-
-  const playGameStructure = () => {
-    const num1 = generateRandomNumber();
-    const num2 = generateRandomNumber();
-    const question = `${num1} ${num2}`;
-
-    const correctAnswer = String(calculateGcd(num1, num2));
-    return [question, correctAnswer];
-  };
-
   playGame(gameTerms, playGameStructure);
 };
